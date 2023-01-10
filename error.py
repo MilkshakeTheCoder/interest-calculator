f33967dddcacbd4ddbbcce9f91629f33cc898f04
MilkshakeTheCoder <aahils2019@gmail.com>
2023-01-10 12:58:53 -0800

    Delete error

--- error.py
+++
@@ -1,9 +0,0 @@
-from cfic import setup
-
-def errorHandler():
-    try:
-        setup()
-    except ValueError:
-        print("Wrong value buddy.")
-    except TypeError:
-        print("Wrong type buddy.")
