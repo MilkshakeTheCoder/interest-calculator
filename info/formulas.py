a64b40393535bfd1b0ec87e41800efe8857433bd
MilkshakeTheCoder <aahils2019@gmail.com>
2023-01-10 15:53:56 -0800

    Update comments in formulas.py

--- info/formulas.py
+++ info/formulas.py
@@ -1,5 +1,6 @@
 import math
 
+# Simple interest inputs and formula...
 def prt():
     p = int(input("What is your depoist/initial amount? "))
     r = float(input("What is your interest rate as a percentage? "))
@@ -7,6 +8,7 @@ def prt():
     t = int(input("How long has this interest been applied (in years)? "))
     return p, r, t
 
+# Non-continual compound interest inputs and formula...
 def prnt():
     p = int(input("What is your depoist/initial amount? "))
     r = float(input("What is your interest rate as a percentage? "))
@@ -15,6 +17,7 @@ def prnt():
     t = int(input("How long has this interest been applied (in years)? "))
     return p, r, n, t
 
+# Continual Compound interest inputs and formula...
 def pert():
     p = int(input("What is your depoist/initial amount? "))
     e = math.e
